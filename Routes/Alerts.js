5b9223b701cbdba4f5567f44f327a6a861e63c11
Mart Bent <martbent99@gmail.com>
2021-05-20 18:51:59 +0200

    Implemented most device calls, started working on room assignment

--- Routes/Alerts.js
+++ Routes/Alerts.js
@@ -21,23 +21,42 @@ router.get('/call', (req, res) =>{
     var patientID = 0;
 
     let getsql = "SELECT * FROM Patient where patient_room_no = " + roomNum;
-
+  
     conn.query(getsql, function (err, result) 
     {
+        if(result.length < 1)
+        {
+            res.send("No patient found for this room number");
+            return;
+        }
+
         patientID = result[0].patient_id;
-        let sql  = `INSERT INTO Alert (patient_id, alert_message) VALUES (${patientID} ,\'${message}\')`;
+        let sql  = `INSERT INTO Alert (patient_id, alert_message) VALUES (${patientID} ,\"${message}\")`;
 
         conn.query(sql, function (err, result) 
         {
             if (err){console.log(err); return;}
 
-            console.log();
-            notifier.notifyWatches(roomNum, message, 0);
+            console.log(result);
+            notifier.notifyWatches(result.insertId, roomNum, message, 0);
             res.send("OK");
         });
     });
 });
 
-
-
+router.put('/:alertID/mark', (req, res) =>{
+    let sql  = "UPDATE Alert SET helped = 1 WHERE alert_id = " + req.params.alertID;
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        if(result.changedRows == 0)
+        {
+            res.send("The patient is already being helped!");
+        }
+        else
+        {
+            res.send("You have been assigned to this patient.");
+            notifier.notifyWatches(req.params.alertID, 0, "None", 1);
+        }
+    });
+});
 module.exports = router;
--- Routes/Commands.js
+++ Routes/Commands.js
@@ -1,9 +1,10 @@
 var express = require('express');
 const router = express.Router();
 var db = require('../SqlHelper');
+var conn = db.getConnection();
 var notifier = require('../Notifier');
 
-router.post('/measurement', (req, res) =>{
+router.get('/measurement', (req, res) =>{
     if(!req.query.roomNumber || req.query.roomNumber === "")
     {
         res.send("ERROR: NO ROOM NUMBER");
@@ -11,22 +12,35 @@ router.post('/measurement', (req, res) =>{
     }
 
     var roomNum = req.query.roomNumber;
+    console.log("Taking a measurement in room: " + roomNum)
     notifier.notifyRobot(roomNum,"MEASUREMENT");
     res.send("OK");
 
 });
 
-router.post('/medicine', (req, res) =>{
-    if(!req.query.roomNumber || req.query.roomNumber === "")
+router.get('/medicine', (req, res) =>{
+    if(!req.query.amazonID || req.query.amazonID === "")
     {
         res.send("ERROR: NO ROOM NUMBER");
         return;
     }
+    console.log(req.query.amazonID);
+    
+    conn.query('SELECT room_no FROM AmazonIdRoomNumber where amazon_id="'+req.query.amazonID+'"', function (err, result) 
+    {
+        if(result.length < 1)
+        {
+            console.log(result);
+            res.send("No room found with this device");
+            return;
+        }
+        console.log(result);
+        var roomNum = req.query.roomNumber;
+        console.log("Deploying medicine in room: " + roomNum);
+        notifier.notifyRobot(roomNum,"MEDICINE");
+        res.send("OK");
 
-    var roomNum = req.query.roomNumber;
-    notifier.notifyRobot(roomNum,"MEDICINE");
-    res.send("OK");
-
+    });
 });
 
 
