47757f464f1df666a3b58107b85b1ea9c1d9b210
Mart Bent <martbent99@gmail.com>
2021-05-23 19:05:42 +0200

    implemented medicine time routes

--- Routes/Nurses.js
+++ Routes/Nurses.js
@@ -1,22 +1,48 @@
 var express = require('express');
 const router = express.Router();
-
-var sqlhelp = require('../SqlHelper');
-const conn = sqlhelp.getConnection();
+var db = require('../SqlHelper');
+var conn = db.getConnection();
 
 router.get('/', (req, res) =>{
       conn.query("SELECT * FROM Nurse", function (err, result) {
-      if (err) throw err;
       res.send(result);
     });
  });
- router.get('/:nurseID', (req, res) =>{
- 
-   let sql  = "SELECT * FROM Nurse WHERE nurse_id ="+req.params.nurseID;
-    conn.query(sql, function (err, result) {
-      if (err) throw err;
-      res.send(result);
-    });
+
+router.get('/:nurseID', (req, res) =>{
+
+  let sql  = "SELECT * FROM Nurse WHERE nurse_id ="+req.params.nurseID;
+  conn.query(sql, function (err, result) {
+    res.send(result);
   });
+});
+
+router.post('/', (req, res) =>{
+let sql  = `INSERT INTO Nurse (nurse_firstname, nurse_lastname, nurse_function) VALUES("${req.query.nurseFirstname}", "${req.query.nurseLastname}","${req.query.nurseFunction}")`;
+conn.query(sql, function (err, result) {
+        if(!err)
+        {
+          res.send("OK");
+        }
+        else
+        {
+             res.send(err);
+        }
+    });
+});
+
+router.delete('/:nurseID', (req, res) =>{
+  let sql  = `DELETE FROM Nurse where nurse_id = ${req.params.nurseID}`;
+  conn.query(sql, function (err, result) {
+         if(!err)
+         {
+             res.send("OK");
+         }
+         else
+         {
+             res.send(err);
+        }
+    });
+});
 
 module.exports = router;
--- Routes/Patients.js
+++ Routes/Patients.js
@@ -1,10 +1,46 @@
 var express = require('express');
 const router = express.Router();
 var db = require('../SqlHelper');
+var conn = db.getConnection();
 
 router.get('/', (req, res) =>{
-    var result = db.query("SELECT * FROM Patient");
-    res.send(result);
+    conn.query("SELECT * FROM Patient", function (err, result) {
+        res.send(result);
+    });
+});
+
+router.get('/:patientID', (req, res) =>{
+    conn.query("SELECT * FROM patient WHERE patient_id =" + req.params.patientID, function (err, result) {
+        res.send(result);
+    });
+});
+
+router.post('/', (req, res) =>{
+let sql  = `INSERT INTO Patient (patient_firstname, patient_lastname, patient_room_no) VALUES("${req.query.patientFirstname}", "${req.query.patientLastname}",${req.query.roomNumber})`;
+conn.query(sql, function (err, result) {
+        if(!err)
+        {
+            res.send("OK");
+        }
+        else
+        {
+            res.send(err);
+        }
+    });
+});
+
+router.delete('/:nurseID', (req, res) =>{
+let sql  = `DELETE FROM Patient where patient_id = ${req.params.patientID}`;
+conn.query(sql, function (err, result) {
+        if(!err)
+        {
+            res.send("OK");
+        }
+        else
+        {
+            res.send(err);
+        }
+    });
 });
 
 module.exports = router;
